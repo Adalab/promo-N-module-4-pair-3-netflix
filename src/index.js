c643e2fd914c4a8307bc5a9c226457ca6ff5ee1a
Ana Montegrifo <amontegrifo@gmail.com>
2021-10-14 11:22:03 +0200

    add render

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-const express = require("express");
-const cors = require("cors");
-const movies = require("./data/movies.json");
-const users = require("./data/users.json");
+const express = require('express');
+const cors = require('cors');
+const movies = require('./data/movies.json');
+const users = require('./data/users.json');
 
 // create and config server
 const server = express();
@@ -11,26 +11,40 @@ server.use(express.json());
 // init express aplication
 const serverPort = 4000;
 server.listen(serverPort, () => {
-  console.log(`Server listening at http://localhost:${serverPort}`);
+	console.log(`Server listening at http://localhost:${serverPort}`);
 });
+
+// set template engine middlewares
+server.set('view engine', 'ejs');
 
 // API
-server.get("/movies", (req, res) => {
-  const response = {
-    success: true,
-    movies,
-  };
-  res.json(response);
+server.get('/movies', (req, res) => {
+	const response = {
+		success: true,
+		movies,
+	};
+	res.json(response);
 });
 
-server.post("/users", (req, res) => {
-  console.log(req.body);
+server.post('/users', (req, res) => {
+	console.log(req.body);
+});
+
+//Endpoints
+server.get('/movie/:movieId', (req, res) => {
+	const foundMovies = movies.find((movie) => movie.id === req.params.movieId);
+	console.log('movie data', foundMovies);
+	res.render('movie', foundMovies);
 });
 
 // STATIC SERVER: listen files in public folder
-const staticServerPath = "./src/public-react"; // relative to the root of the project
+const staticServerPath = './src/public-react'; // relative to the root of the project
 server.use(express.static(staticServerPath));
 
 // STATIC SERVER IMG: listen files in public folder
-const staticServerImgPath = "./src/public-movies-images"; // relative to the root of the project
+const staticServerImgPath = './src/public-movies-images'; // relative to the root of the project
 server.use(express.static(staticServerImgPath));
+
+// STATIC SERVER STYLES
+
+server.use(express.static('./src/public-react/static'));
