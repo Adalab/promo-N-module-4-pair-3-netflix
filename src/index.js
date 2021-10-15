cccdce85fa43b446a9c7413e4502d00444e636d1
monipeny <mlamasrial@gmail.com>
2021-10-15 11:22:13 +0200

    database

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
-const express = require('express');
-const cors = require('cors');
-const movies = require('./data/movies.json');
-const users = require('./data/users.json');
+const express = require("express");
+const cors = require("cors");
+const movies = require("./data/movies.json");
+const users = require("./data/users.json");
+const Database = require("better-sqlite3");
+const db = new Database("./src/db/database.db", { verbose: console.log });
 
 // create and config server
 const server = express();
@@ -11,40 +13,39 @@ server.use(express.json());
 // init express aplication
 const serverPort = 4000;
 server.listen(serverPort, () => {
-	console.log(`Server listening at http://localhost:${serverPort}`);
+  console.log(`Server listening at http://localhost:${serverPort}`);
 });
 
 // set template engine middlewares
-server.set('view engine', 'ejs');
+server.set("view engine", "ejs");
 
 // API
-server.get('/movies', (req, res) => {
-	const response = {
-		success: true,
-		movies,
-	};
-	res.json(response);
+server.get("/movies", (req, res) => {
+  const query = db.prepare("SELECT * FROM movies");
+  const movies = query.all();
+  console.log(movies);
+  res.json(movies);
 });
 
-server.post('/users', (req, res) => {
-	console.log(req.body);
+server.post("/users", (req, res) => {
+  console.log(req.body);
 });
 
 //Endpoints
-server.get('/movie/:movieId', (req, res) => {
-	const foundMovies = movies.find((movie) => movie.id === req.params.movieId);
-	console.log('movie data', foundMovies);
-	res.render('movie', foundMovies);
+server.get("/movie/:movieId", (req, res) => {
+  const foundMovies = movies.find((movie) => movie.id === req.params.movieId);
+  console.log("movie data", foundMovies);
+  res.render("movie", foundMovies);
 });
 
 // STATIC SERVER: listen files in public folder
-const staticServerPath = './src/public-react'; // relative to the root of the project
+const staticServerPath = "./src/public-react"; // relative to the root of the project
 server.use(express.static(staticServerPath));
 
 // STATIC SERVER IMG: listen files in public folder
-const staticServerImgPath = './src/public-movies-images'; // relative to the root of the project
+const staticServerImgPath = "./src/public-movies-images"; // relative to the root of the project
 server.use(express.static(staticServerImgPath));
 
 // STATIC SERVER STYLES
 
-server.use(express.static('./src/public-react/static'));
+server.use(express.static("./src/public-react/static"));
